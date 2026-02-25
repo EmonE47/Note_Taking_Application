733a40aee9311028c96ecb9b0e7c1379fb5cc149
Emon <mdahsanulislamemon@gmail.com>
2026-02-25 13:34:51 +0600

    Added 120Hz compatibility

--- android/app/src/main/kotlin/com/example/my_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/my_app/MainActivity.kt
@@ -1,5 +1,32 @@
 package com.example.my_app
 
+import android.os.Build
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onResume() {
+        super.onResume()
+        requestHighestRefreshRate()
+    }
+
+    private fun requestHighestRefreshRate() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return
+        }
+
+        @Suppress("DEPRECATION")
+        val display = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            this.display ?: windowManager.defaultDisplay
+        } else {
+            windowManager.defaultDisplay
+        } ?: return
+
+        val bestMode = display.supportedModes.maxByOrNull { it.refreshRate } ?: return
+        val params = window.attributes
+
+        if (params.preferredDisplayModeId != bestMode.modeId) {
+            params.preferredDisplayModeId = bestMode.modeId
+            window.attributes = params
+        }
+    }
+}
